413c22ba000d965c3a5e1ec00e5d0a9fbcf6d289
jaspreet tpss <jaspreetstpss@gmail.com>
2025-07-17 23:54:28 +0530

    the hero  and about section design.

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,14 @@
+// musicbar
+
+gsap.to(".shaking-img", {
+  x: 10,
+  duration: 2,
+  repeat: -1,
+  yoyo: true,
+  ease: "sine.inOut",
+});
+
+// navbar
 const menuBtn = document.getElementById("menu-btn");
 const navPopup = document.getElementById("nav-popup");
 
@@ -36,7 +47,7 @@ links.forEach((link) => {
       duration: 0.4,
       scale: 1.1,
       letterSpacing: "4px",
-      color: "#1e2376", // optional: purple on hover
+      color: "#8b7cde", // optional: purple on hover
       ease: "power2.out",
     });
   });
@@ -50,4 +61,70 @@ links.forEach((link) => {
       ease: "power2.inOut",
     });
   });
+});
+
+// Animate header elements on page load
+gsap.from("header a", {
+  opacity: 0,
+  y: -30,
+  duration: 1,
+  ease: "power3.out",
+  delay: 0.2,
+});
+
+gsap.from("#menu-btn", {
+  opacity: 0,
+  y: -30,
+  duration: 1,
+  ease: "power3.out",
+  delay: 0.4,
+});
+
+// Animate hero section on page load
+
+const heroTitle = document.querySelector(".hero-title");
+const words = heroTitle.textContent.trim().split(" ");
+heroTitle.innerHTML = words
+  .map((word) => `<span class="word inline-block">${word}&nbsp;</span>`)
+  .join("");
+
+// Step 2: Animate using a GSAP timeline
+const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
+
+tl.from(
+  ".hero-title .word",
+  {
+    opacity: 0,
+    y: 50,
+    duration: 2,
+    stagger: 0.6,
+  },
+  0
+) // start immediately
+
+  .from(
+    ".hero .custom-btn",
+    {
+      opacity: 0,
+      y: 50,
+      duration: 0.6,
+    },
+    "-=0.3"
+  ); // starts slightly
+
+// Feature section animation
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".track-card", {
+  opacity: 0,
+  y: 100,
+  scale: 0.8,
+  duration: 1.5,
+  ease: "elastic.out(1, 0.4)", // <- Elastic bounce effect
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: "#tracks", // ID of the section
+    start: "top 10%", // When 80% into viewport
+    toggleActions: "play none none none",
+  },
 });
